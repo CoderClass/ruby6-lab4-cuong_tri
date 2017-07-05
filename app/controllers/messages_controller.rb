aa98aed7446f5a6076e0649b2ec6f140982c0a15
Tri Ngo <ngominhtrint@gmail.com>
2017-07-05 19:43:23 +0700

    Milestone 2: Basic Chat

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+
+  def index
+    @room = Room.find_by_id(params[:room_id])
+    @messages = @room.messages
+  end
+
+  def create
+    @room = Room.find_by_id(params[:room_id])
+    @message = @room.messages.build message_params
+
+    if @message.save 
+      redirect_to room_messages_path
+    else
+      flash[:notice] = "Error #{@message.error.full_message.to_sentence}"
+      redirect_to room_messages_path
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
